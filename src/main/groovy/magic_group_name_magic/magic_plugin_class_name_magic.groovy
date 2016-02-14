4c4316fac83686b4963d11a561585a4f3e49f0f4
Micah Zoltu <micah@zoltu.net>
2016-02-14 15:37:47 -0800

    Creates gradle plugin scaffolding.

---
+++ src/main/groovy/magic_group_name_magic/magic_plugin_class_name_magic.groovy
@@ -0,0 +1,10 @@
+package magic_group_name_magic
+
+import org.gradle.api.Project
+import org.gradle.api.Plugin
+
+class magic_plugin_class_name_magicPlugin implements Plugin<Project> {
+	void apply(Project project) {
+		// TODO: write plugin code here
+	}
+}
