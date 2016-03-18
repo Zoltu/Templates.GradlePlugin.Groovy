d724901866d7588472baba6acab8aef237f76b0e
Micah Zoltu <micah@zoltu.net>
2016-03-17 23:24:51 -0700

    Fixes a bug in the plugin class name.
    The name should match the file name and the reference in the properties file.  Since the magic string asks for a class name, don't add a suffix on behalf of the user.

--- src/main/groovy/magic_group_name_magic/magic_plugin_class_name_magic.groovy
+++ src/main/groovy/magic_group_name_magic/magic_plugin_class_name_magic.groovy
@@ -3,7 +3,7 @@ package magic_group_name_magic
 import org.gradle.api.Project
 import org.gradle.api.Plugin
 
-class magic_plugin_class_name_magicPlugin implements Plugin<Project> {
+class magic_plugin_class_name_magic implements Plugin<Project> {
 	void apply(Project project) {
 		// TODO: write plugin code here
 	}
